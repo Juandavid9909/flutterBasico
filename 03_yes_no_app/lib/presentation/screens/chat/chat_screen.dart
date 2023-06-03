d95858e1966d9f7d1577dbb64e2ba6deebde6069
Juan David <juandavid918@gmail.com>
2023-06-03 13:35:33 -0500

    Mostrando mensajes con estilos aplicados

---
+++ 03_yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: const Padding(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://i.pinimg.com/474x/65/52/8e/65528ef2665f5f8fbc2cd0c5c88a59d1.jpg")),
+          ),
+          title: const Text("Mi amor <3"),
+          centerTitle: false),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return (index % 2 == 0
+                  ? const HerMessageBubble()
+                  : const MyMessageBuble()
+                );
+              }
+            )),
+            Text("Mundo")
+          ],
+        ),
+      ),
+    );
+  }
+}
